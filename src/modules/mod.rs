953b0e56d1655dff4dd6283bf6d3ab6d95885d9a
Astralchroma <astralchroma@proton.me>
2023-11-23 08:38:07 +0000

    dont let other people enable modules lol

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -72,12 +72,12 @@ pub async fn get_formatted_module_list(context: &Context<'_>) -> Result<String> 
 	})
 }
 
-#[command(guild_only, slash_command, subcommands("enable", "list", "disable"))]
+#[command(owners_only, guild_only, slash_command, subcommands("enable", "list", "disable"))]
 pub async fn module(_: Context<'_>) -> Result<()> {
 	Ok(())
 }
 
-#[command(guild_only, slash_command)]
+#[command(owners_only, guild_only, slash_command)]
 async fn enable(context: Context<'_>, module: Modules) -> Result<()> {
 	let guild = context.guild_id().expect("command is guild only");
 	let guild_id = guild.0 as i64;
@@ -91,14 +91,14 @@ async fn enable(context: Context<'_>, module: Modules) -> Result<()> {
 	Ok(())
 }
 
-#[command(guild_only, slash_command)]
+#[command(owners_only, guild_only, slash_command)]
 async fn list(context: Context<'_>) -> Result<()> {
 	let module_list = get_formatted_module_list(&context).await?;
 	context.reply(format!("Enabled Modules: {}.", module_list)).await?;
 	Ok(())
 }
 
-#[command(guild_only, slash_command)]
+#[command(owners_only, guild_only, slash_command)]
 async fn disable(context: Context<'_>, module: Modules) -> Result<()> {
 	let guild = context.guild_id().expect("command is guild only");
 	let guild_id = guild.0 as i64;
